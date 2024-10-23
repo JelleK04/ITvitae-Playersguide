da7ade957a116c10650ce43e3b01dc7ee8a49cca
JelleK04 <jellekruijt@gmail.com>
2024-10-23 15:01:19 +0200

    Added day 5 Part1of2

--- Day 5/src/Part1of2.java
+++ Day 5/src/Part1of2.java
@@ -1,21 +1,22 @@
 public class Part1of2 {
-        public static void main(String[] args) {
-            byte myByte = 127;
-            System.out.println("byte: " + myByte);
-            short myShort = 32767;
-            System.out.println("short: " + myShort);
-            int myInt = 2;
-            System.out.println("int: " + myInt);
-            long myLong = 128000L;
-            System.out.println("long: " + myLong);
-            float myFloat = 8.427f;
-            System.out.println("float: " + myFloat);
-            double myDouble = 23.894d;
-            System.out.println("double: " + myDouble);
-            boolean myBoolean = true;
-            System.out.println("boolean: " + myBoolean);
-            char myChar = 'a';
-            System.out.println("char: " + myChar);
-        }
+    public static void main(String[] args) {
+        byte myByte = -128;
+        System.out.println("byte: " + myByte);
+        short myShort = 22223;
+        System.out.println("short: " + myShort);
+        int myInt = 36;
+        System.out.println("int: " + myInt);
+        long myLong = 235000L;
+        System.out.println("long: " + myLong);
+        float myFloat = 11.792f;
+        System.out.println("float: " + myFloat);
+        double myDouble = 71.294d;
+        System.out.println("double: " + myDouble);
+        boolean myBoolean = false;
+        System.out.println("boolean: " + myBoolean);
+        char myChar = 'b';
+        System.out.println("char: " + myChar);
+        String myString = "Example text";
+        System.out.println("String: " + myString);
+    }
 }
-
--- Day 5/src/Part2of2.java
+++ Day 5/src/Part2of2.java
@@ -1,2 +1,5 @@
 public class Part2of2 {
+    public static void main(String[] args) {
+
+    }
 }
