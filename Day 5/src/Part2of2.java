3489451684b79d6fbcbf7cb46ca49823e202ed2c
JelleK04 <jellekruijt@gmail.com>
2024-10-25 13:20:20 +0200

    Added day 5 Part2of2

--- Day 5/src/Part2of2.java
+++ Day 5/src/Part2of2.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Part2of2 {
     public static void main(String[] args) {
-
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("What is the base of the triangle?");
+        float base = myScanner.nextFloat();
+        System.out.println("What is the height of the triangle?");
+        float height = myScanner.nextFloat();
+        System.out.println("The area of the triangle is " + base*height/2);
     }
 }
