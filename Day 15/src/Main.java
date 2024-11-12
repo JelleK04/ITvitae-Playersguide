44c7ed967fcaf43432ad815c2c335a006315751f
JelleK04 <jellekruijt@gmail.com>
2024-11-12 11:23:52 +0100

    Added day 15

--- Day 15/src/Main.java
+++ Day 15/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-
+    public static void main(String[] args) {
+    int[] array = { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+    //calculates and prints smallest number in array
+    int currentSmallest = Integer.MAX_VALUE;
+    for (int i  : array) {
+        if (i < currentSmallest) {
+            currentSmallest = array[i];
+        }
+    }
+        System.out.println(currentSmallest);
+    //adds all number in the array up
+    int total = 0;
+    for (int j : array) {
+        total += j;
+    }
+    //calculate and print average of all numbers in the array
+        double average = (double)total / array.length;
+        System.out.println(average);
+    }
 }
 
 
