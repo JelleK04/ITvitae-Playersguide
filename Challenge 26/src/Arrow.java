6b79a28b785e2d9a47d89233479ce8a7d6b3d712
JelleK04 <jellekruijt@gmail.com>
2024-12-16 15:33:21 +0100

    Added Challenge 26

--- Challenge 26/src/Arrow.java
+++ Challenge 26/src/Arrow.java
@@ -1,2 +1,83 @@
 public class Arrow {
+    enum Arrowhead {
+        STEEL,
+        WOOD,
+        OBSIDIAN,
+
+    }
+
+    enum Fletching {
+        PLASTIC,
+        TURKEYFEATHERS,
+        GOOSEFEATHERS,
+    }
+   private Arrow.Arrowhead arrowhead;
+   private Arrow.Fletching fletching;
+   private float lengthInCm;
+
+    Arrow (String arrowheadInput, String fletchingInput, float lengthInCmInput) {
+        switch (arrowheadInput.toLowerCase().trim()) {
+            case "steel":
+                arrowhead = Arrow.Arrowhead.STEEL;
+                break;
+            case "wood":
+                arrowhead = Arrow.Arrowhead.WOOD;
+                break;
+            case "obsidian":
+                arrowhead = Arrow.Arrowhead.OBSIDIAN;
+                break;
+        }
+        switch (fletchingInput.toLowerCase().trim()) {
+            case "plastic":
+                fletching = Arrow.Fletching.PLASTIC;
+                break;
+            case "turkeyfeathers":
+                fletching = Arrow.Fletching.TURKEYFEATHERS;
+                break;
+            case "goosefeathers":
+                fletching = Arrow.Fletching.GOOSEFEATHERS;
+                break;
+        }
+        lengthInCm = lengthInCmInput;
+    }
+
+    public Arrowhead getArrowhead () {
+        return arrowhead;
+    }
+
+    public Fletching getFletching () {
+        return fletching;
+    }
+
+    public float getLengthInCm () {
+        return lengthInCm;
+    }
+
+    float getCost(Arrowhead arrowhead, Fletching fletching, float length) {
+        float gold = 0f;
+        switch (arrowhead) {
+            case STEEL:
+                gold = 10;
+                break;
+            case WOOD:
+                gold = 3;
+                break;
+            case OBSIDIAN:
+                gold = 5;
+                break;
+        }
+        switch (fletching) {
+            case PLASTIC:
+                gold = gold + 10;
+                break;
+            case TURKEYFEATHERS:
+                gold = gold + 5;
+                break;
+            case GOOSEFEATHERS:
+                gold = gold + 3;
+                break;
+        }
+        gold = (gold + (length * 0.05f));
+        return gold;
+    }
 }
--- Challenge 26/src/Main.java
+++ Challenge 26/src/Main.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("What arrowhead do you want?");
+        System.out.println("We offer steel, wood and obsidian.");
+        String arrowheadInput = myScanner.nextLine();
+        System.out.println("What fletching type do you want?");
+        System.out.println("We offer plastic, turkeyfeathers and goosefeathers.");
+        String flethingInput = myScanner.nextLine();
+        System.out.println("How long do you want your arrow to be?");
+        float lengthInCmInput;
+        do {
+            System.out.println("We can make it 60 to 100 cm long.");
+            lengthInCmInput = myScanner.nextFloat();
+        }
+        while (lengthInCmInput < 60 || lengthInCmInput > 100);
+        Arrow yourArrow = new Arrow(arrowheadInput, flethingInput, lengthInCmInput);
+        System.out.println("your arrow costs " + yourArrow.getCost(yourArrow.getArrowhead(), yourArrow.getFletching(), yourArrow.getLengthInCm()) + " gold.");
+    }
 }
